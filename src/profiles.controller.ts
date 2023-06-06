061c2f1f88263acc7222773f894c91cf2222a68c
JcJet <jcjet88@gmail.com>
2023-06-06 16:10:21 +0300

    typing

--- src/profiles.controller.ts
+++ src/profiles.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, UseFilters} from '@nestjs/common';
+import { Controller, UseFilters } from '@nestjs/common';
 import { ProfilesService } from './profiles.service';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 import { Profile } from './profiles.entity';
 import { CreateProfileDto } from './dto/createProfile.dto';
 import { LoginDto } from './dto/login.dto';
 import { HttpExceptionFilter } from './http-exception.filter';
+import { CreateUserResultDto } from './dto/create-user-result.dto';
+import {DeleteResult, UpdateResult} from "typeorm";
 
 @Controller()
 @UseFilters(new HttpExceptionFilter())
@@ -12,28 +14,35 @@ export class ProfilesController {
   constructor(private readonly profilesService: ProfilesService) {}
 
   @MessagePattern({ cmd: 'registration' })
-  async registration(@Payload() data: { registrationDto: CreateProfileDto }) {
+  async registration(
+    @Payload() data: { registrationDto: CreateProfileDto },
+  ): Promise<{
+    profile: Profile;
+    tokens: { accessToken: string; refreshToken: string };
+  }> {
     return await this.profilesService.registration(data.registrationDto);
   }
 
   @MessagePattern({ cmd: 'login' })
   async login(
     @Payload() data: { loginDto: LoginDto },
-  ): Promise<{ token: string }> {
+  ): Promise<CreateUserResultDto> {
     return await this.profilesService.login(data.loginDto);
   }
 
   @MessagePattern({ cmd: 'logout' })
-  async logout(@Payload() data: { refreshToken: string }) {
+  async logout(@Payload() data: { refreshToken: string }): Promise<DeleteResult> {
     return await this.profilesService.logout(data.refreshToken);
   }
   @MessagePattern({ cmd: 'refreshAccessToken' })
-  async refresh(@Payload() data: { refreshToken: string }) {
+  async refresh(
+    @Payload() data: { refreshToken: string },
+  ): Promise<CreateUserResultDto> {
     return await this.profilesService.refresh(data.refreshToken);
   }
 
   @MessagePattern({ cmd: 'activate' })
-  async activate(@Payload() data: { activationLink: string }) {
+  async activate(@Payload() data: { activationLink: string }): Promise<void> {
     return await this.profilesService.activate(data.activationLink);
   }
   @MessagePattern({ cmd: 'getAllProfiles' })
@@ -49,7 +58,7 @@ export class ProfilesController {
       updateProfileDto: CreateProfileDto;
       avatarFileName: string;
     },
-  ): Promise<Profile> {
+  ): Promise<UpdateResult> {
     return await this.profilesService.updateProfile(
       data.profileId,
       data.updateProfileDto,
@@ -58,20 +67,20 @@ export class ProfilesController {
   }
 
   @MessagePattern({ cmd: 'deleteProfile' })
-  async delete(@Payload() data: { profileId: number }): Promise<Profile> {
+  async delete(@Payload() data: { profileId: number }): Promise<DeleteResult> {
     return await this.profilesService.deleteProfile(data.profileId);
   }
 
   @MessagePattern({ cmd: 'getProfileById' })
-  async getProfileById(@Payload() data: { profileId: number }) {
+  async getProfileById(@Payload() data: { profileId: number }): Promise<Profile> {
     return await this.profilesService.getProfileById(data.profileId);
   }
   @MessagePattern({ cmd: 'getProfileByUserId' })
-  async getProfileByUserId(@Payload() data: { userId: number }) {
+  async getProfileByUserId(@Payload() data: { userId: number }): Promise<Profile> {
     return await this.profilesService.getProfileByUserId(data.userId);
   }
   @MessagePattern({ cmd: 'loginVk' })
-  async loginVk(@Payload() data: { code: string }) {
+  async loginVk(@Payload() data: { code: string }): Promise<CreateUserResultDto> {
     return this.profilesService.loginVk(data.code);
   }
 }
